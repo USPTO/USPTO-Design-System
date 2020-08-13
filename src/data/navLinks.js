a95bcd7c071271e74901fea76435b676fe8ee8d7
Menka Kayan <menka.kayan@uspto.gov>
2020-08-13 10:22:27 -0400

    cleaning up templates

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -2,10 +2,12 @@
 Object.assign(exports, {
   componentNavLinks: [
     { href: 'components.html', text: 'Components' },
-    { href: 'badges.html', text: 'Badges' }
+    { href: 'badges.html', text: 'Badges' },
+    { href: 'buttons.html', text: 'Buttons' }
   ],
   pageTemplateNavLinks: [
     { href: 'pageTemplate1.html', text: 'Page Template 1' },
+    { href: 'pageTemplate2.html', text: 'Page Template 2' },
     { href: 'loader.html', text: 'Loading Spinner' }
   ]
 })
