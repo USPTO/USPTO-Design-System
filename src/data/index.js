57768da28c9f2fda698f4d81f2105ca5d4075cf9
Menka Kayan <menka.kayan@uspto.gov>
2020-08-24 11:07:46 -0400

    updating left nav template

--- src/data/index.js
+++ src/data/index.js
@@ -3,15 +3,15 @@ const navLinks = require('./navLinks')
 
 module.exports = {
 
-  
+
 
   description: pkg.description,
 
 
-  links: {    
+  links: {
     changelog: 'changelog.html',
-   // firstComponent: `components/${sidenav[0].children[0].href}`,
-    home: 'index.html',    
+    // firstComponent: `components/${sidenav[0].children[0].href}`,
+    home: 'index.html',
   },
 
   title: 'USPTO UI Design Library',
@@ -22,7 +22,7 @@ module.exports = {
         href: '',
         text: 'Download',
         title: 'Download',
-      }      
+      }
     ]
   },
 
@@ -32,33 +32,33 @@ module.exports = {
         href: 'howto.html',
         text: 'How to use DPL',
         title: 'How to use DPL',
-      } , 
+      },
       {
-      href: 'foundation/foundation.html',
+        href: 'foundation/foundation.html',
         text: 'Foundation',
         title: 'Foundation',
-    },
+      },
 
       {
         href: 'components/components.html',
         text: 'Components',
         title: 'Components',
-      } , 
+      },
       {
         href: 'patterns/patterns.html',
-      text: 'Patterns',
-      title: 'Patterns',
-    },
+        text: 'Patterns',
+        title: 'Patterns',
+      },
       {
         href: 'pageTemplates/pageTemplate1.html',
         text: 'Page templates',
         title: 'Page templates',
-      } ,
+      },
       {
         href: 'about.html',
         text: 'About',
         title: 'About',
-      }      
+      }
     ]
   },
 
@@ -66,6 +66,6 @@ module.exports = {
 
   ...navLinks,
 
-  
+
 
 }
