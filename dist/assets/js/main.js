30e613ac10d56a709fcb7d5851d0976dee58da0c
Menka Kayan <menka.kayan@uspto.gov>
2020-08-31 11:37:53 -0400

    adding clipboard and updating side nav

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,59 +1,31 @@
 // just a static asset for now
 $(function () {
-
-  $('.current-year').text(new Date().getFullYear())
-
   var file = location.pathname.split('/').pop() // set `current` page
   $('a[href="' + file + '"]').parent('li').addClass('active')
   $('a[href="' + file + '"]').attr('aria-current', 'page')
 
-  $('.toast').toast('show')
+  var clipboard = new ClipboardJS('.clipboard');
 
-  $('[data-toggle="tooltip"]').tooltip()
+  clipboard.on('success', function (e) {
+    $(e.trigger).text("Copied!");
+    e.clearSelection();
+    setTimeout(function () {
+      $(e.trigger).text("Copy Code");
+    }, 2500);
+  });
 
-  sidenavBehavior()
 
+  $("#leftSideNav").change(function () {
+    location.href = $(this).val();
+    var file = location.pathname.split('/').pop()
+    console.log(location.href)
+    console.log(file)
+})
 })
 
-function $controls (el) {
+
+
+function $controls(el) {
   return $('#' + el.getAttribute('aria-controls'))
-}
+};
 
-function sidenavBehavior () {
-
-  var classCollapsed = 'sidenav--collapsed'
-  var sidenavs = $('.sidenav')
-  var sidenavToggles = $('.sidenav__toggle')
-  var mqList = window.matchMedia('(min-width: 992px)') // sm=576, md=768, lg=992
-
-  var autoCollapse = function (e) {
-    sidenavs.toggleClass(classCollapsed, e.matches === false)
-    sidenavs.each(function () { updateState($(this)) })
-  }
-
-  var updateState = function (nav) {
-    var collapsed = nav.hasClass(classCollapsed)
-    sidenavToggles
-      .filter('[aria-controls="' + nav.attr('id') + '"]')
-      .attr('aria-expanded', !collapsed)
-    if (collapsed) {
-      // expand section[aria-expanded]
-      nav.find('.sidenav__section__toggle.collapsed').click()
-    }
-  }
-
-  mqList.addListener(autoCollapse)
-  autoCollapse(mqList)
-
-  sidenavToggles.each(function () {
-    updateState($controls(this))
-  })
-
-  sidenavToggles.on('click', function () {
-    // might consider turning off auto-collapse if user interacts?
-    var nav = $controls(this)
-    nav.toggleClass(classCollapsed)
-    updateState(nav)
-  })
-
-}
