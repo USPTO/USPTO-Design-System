a19b290c07fa8c922e0088714fc7343e1fd93e62
Zoin Amir <zoin.amir@uspto.gov>
2021-06-07 10:17:29 -0400

    form hint and credit card validation

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -62,6 +62,17 @@ $(function () {
   $('body').on('click', '#settingExample1 .dropdown-item', function (e) {  
     $('#settingExample1 .dropdown-item').removeClass('active');  
   });
+  /*
+   * Credit card validation
+   */
+
+  $('.creditCardText').keyup(function() {
+    var foo = $(this).val().split("-").join(""); // remove hyphens
+    if (foo.length > 0) {
+      foo = foo.match(new RegExp('.{1,4}', 'g')).join("-");
+    }
+    $(this).val(foo);
+  });
 
 })
 
